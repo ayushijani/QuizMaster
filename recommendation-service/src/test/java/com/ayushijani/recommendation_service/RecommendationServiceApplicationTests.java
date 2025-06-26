1131c7946c9b5eb87d52e9feb05f6a74b921fe3a
Ayushi <ayushiiijani@gmail.com>
2025-06-26 19:16:08 +0530

    Create recommendatition-service

---
+++ recommendation-service/src/test/java/com/ayushijani/recommendation_service/RecommendationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayushijani.recommendation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
